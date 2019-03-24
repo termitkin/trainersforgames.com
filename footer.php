2869ea284d12ef9c71bc40420bb8c7b0228fb1f9
termitkin <bigos911@gmail.com>
2019-03-24 21:21:48 +0300

    tag fix

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
   <link href="//trainersforgames.com/wp-content/themes/trainersforgames-v1/style.css" rel="stylesheet">
 
-  <footer class="footer__inner">
+  <div class="footer__inner">
 		<div class="footer__links-wrap footer__links-wrap_links-align_left">
       <a class="footer__link" href="/" title="Home page">Home</a>
       <a class="footer__link" href="//savesforgames.com/" title="Download Saves For Games" rel="nofollow noopener" target="_blank">Saves&nbsp;For&nbsp;Games</a>
@@ -11,7 +11,7 @@
       <span>Powered&nbsp;By&nbsp;<a class="footer__link" href="http://wordpress.org/" title="Powered By WordPress" rel="nofollow" target="_blank">WordPress</a></span>
       <span>&copy; 2018</span>
     </div>
-  </footer>
+  </div>
 
 </footer>
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>

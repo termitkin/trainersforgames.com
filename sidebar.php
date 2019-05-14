e8caf6f65c77545d0848248b65ac625127669cab
termitkin <bigos911@gmail.com>
2019-05-14 20:41:11 +0300

    links addresses fix

--- sidebar.php
+++ sidebar.php
@@ -3,34 +3,34 @@
 		<div class="popular-posts-heading">Most Popular Savegame</div>
 		<ul class="popular-posts-list">
 			<li>
-				<a class="popular-posts-list__link" href="/game-saves/save-for-nfs-underground-2/" title="Save for NFS Underground 2">NFS Underground 2</a> - 322,244 views
+				<a class="popular-posts-list__link" href="https://savesforgames.com/game-saves/save-for-nfs-underground-2/" title="Save for NFS Underground 2">NFS Underground 2</a> - 350,736 views
 			</li>
 			<li>
-				<a class="popular-posts-list__link" href="/game-saves/save-for-test-drive-unlimited-2/" title="Save for Test Drive Unlimited 2">Test Drive Unlimited 2</a> - 263,138 views
+				<a class="popular-posts-list__link" href="https://savesforgames.com/game-saves/save-for-test-drive-unlimited-2/" title="Save for Test Drive Unlimited 2">Test Drive Unlimited 2</a> - 269,937 views
 			</li>
 			<li>
-				<a class="popular-posts-list__link" href="/game-saves/save-for-mafia-2/" title="Save for Mafia 2">Mafia 2</a> - 143,555 views
+				<a class="popular-posts-list__link" href="https://savesforgames.com/game-saves/save-for-mafia-2/" title="Save for Mafia 2">Mafia 2</a> - 148,754 views
 			</li>
 			<li>
-				<a class="popular-posts-list__link" href="/game-saves/save-for-bully-scholarship-edition/" title="Save for Bully Scholarship Edition">Bully Scholarship Edition</a> - 134,626 views
+				<a class="popular-posts-list__link" href="https://savesforgames.com/game-saves/save-for-bully-scholarship-edition/" title="Save for Bully Scholarship Edition">Bully Scholarship Edition</a> - 141,513 views
 			</li>
 			<li>
-				<a class="popular-posts-list__link" href="/game-saves/save-for-grand-theft-auto-5/" title="Save for Grand Theft Auto 5">Grand Theft Auto 5</a> - 114,955 views
+				<a class="popular-posts-list__link" href="https://savesforgames.com/game-saves/save-for-grand-theft-auto-5/" title="Save for Grand Theft Auto 5">Grand Theft Auto 5</a> - 129,956 views
 			</li>
 			<li>
-				<a class="popular-posts-list__link" href="/game-saves/save-for-assassins-creed-unity/" title="Save for Assassin’s Creed: Unity">Assassin&apos;s Creed: Unity</a> - 91,390 views
+				<a class="popular-posts-list__link" href="https://savesforgames.com/game-saves/save-for-assassins-creed-unity/" title="Save for Assassin’s Creed: Unity">Assassin&apos;s Creed: Unity</a> - 92,902 views
 			</li>
 			<li>
-				<a class="popular-posts-list__link" href="/game-saves/save-for-batman-arkham-city/" title="Save for Batman Arkham City">Batman Arkham City</a> - 83,661 views
-			</li>
-		    <li>
-			    <a class="popular-posts-list__link" href="/game-saves/save-for-devil-may-cry-4/" title="Save for Devil May Cry 4">Devil May Cry 4</a> - 82,732 views
-		    </li>
-			<li>
-				<a class="popular-posts-list__link" href="/game-saves/save-for-assassins-creed-3/" title="Save for Assassin’s Creed 3">Assassin&apos;s Creed 3</a> - 79,537 views
+				<a class="popular-posts-list__link" href="https://savesforgames.com/game-saves/save-for-devil-may-cry-4/" title="Save for Devil May Cry 4">Devil May Cry 4</a> - 87,415 views
 			</li>
 			<li>
-				<a class="popular-posts-list__link" href="/game-saves/save-for-colin-mcrae-dirt-3/" title="Save for Colin McRae DiRT 3">Colin McRae DiRT 3</a> - 79,262 views
+				<a class="popular-posts-list__link" href="https://savesforgames.com/game-saves/save-for-batman-arkham-city/" title="Save for Batman Arkham City">Batman Arkham City</a> - 85,707 views
+			</li>
+			<li>
+				<a class="popular-posts-list__link" href="https://savesforgames.com/game-saves/save-for-assassins-creed-3/" title="Save for Assassin’s Creed 3">Assassin&apos;s Creed 3</a> - 85,333 views
+			</li>
+			<li>
+				<a class="popular-posts-list__link" href="https://savesforgames.com/game-saves/save-for-colin-mcrae-dirt-3/" title="Save for Colin McRae DiRT 3">Colin McRae DiRT 3</a> - 81,513 views
 			</li>
 		</ul>
 	<!--/noindex-->

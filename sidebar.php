a61e3062640a473c0ec7d0297132a78d115646e9
termitkin <bigos911@gmail.com>
2022-01-06 12:15:14 +0300

    update sidebar info

--- sidebar.php
+++ sidebar.php
@@ -3,34 +3,34 @@
 		<div class="popular-posts-heading">Most Popular Savegame</div>
 		<ul class="popular-posts-list">
 			<li>
-				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-nfs-underground-2/" title="Save for NFS Underground 2">NFS Underground 2</a> - 377,161 views
+				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-nfs-underground-2/" title="Save for NFS Underground 2">NFS Underground 2</a> - 536,838 views
 			</li>
 			<li>
-				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-test-drive-unlimited-2/" title="Save for Test Drive Unlimited 2">Test Drive Unlimited 2</a> - 276,523 views
+				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-test-drive-unlimited-2/" title="Save for Test Drive Unlimited 2">Test Drive Unlimited 2</a> - 316,590 views
+			</li>
+            <li>
+                <a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-grand-theft-auto-5/" title="Save for Grand Theft Auto 5">Grand Theft Auto 5</a> - 313,912 views
+            </li>
+			<li>
+				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-mafia-2/" title="Save for Mafia 2">Mafia 2</a> - 201,979 views
 			</li>
 			<li>
-				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-mafia-2/" title="Save for Mafia 2">Mafia 2</a> - 154,474 views
+				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-bully-scholarship-edition/" title="Save for Bully Scholarship Edition">Bully Scholarship Edition</a> - 190,128 views
+			</li>
+            <li>
+                <a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-assassins-creed-3/" title="Save for Assassin’s Creed 3">Assassin&apos;s Creed 3</a> - 111,354 views
+            </li>
+            <li>
+                <a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-devil-may-cry-4/" title="Save for Devil May Cry 4">Devil May Cry 4</a> - 110,076 views
+            </li>
+            <li>
+                <a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-batman-arkham-city/" title="Save for Batman Arkham City">Batman Arkham City</a> - 104,943 views
+            </li>
+			<li>
+				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-assassins-creed-unity/" title="Save for Assassin’s Creed: Unity">Assassin&apos;s Creed: Unity</a> - 99,821 views
 			</li>
 			<li>
-				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-grand-theft-auto-5/" title="Save for Grand Theft Auto 5">Grand Theft Auto 5</a> - 148,885 views
-			</li>
-			<li>
-				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-bully-scholarship-edition/" title="Save for Bully Scholarship Edition">Bully Scholarship Edition</a> - 148,453 views
-			</li>
-			<li>
-				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-assassins-creed-unity/" title="Save for Assassin’s Creed: Unity">Assassin&apos;s Creed: Unity</a> - 93,990 views
-			</li>
-			<li>
-				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-devil-may-cry-4/" title="Save for Devil May Cry 4">Devil May Cry 4</a> - 90,607 views
-			</li>
-			<li>
-				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-batman-arkham-city/" title="Save for Batman Arkham City">Batman Arkham City</a> - 87,994 views
-			</li>
-			<li>
-				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-assassins-creed-3/" title="Save for Assassin’s Creed 3">Assassin&apos;s Creed 3</a> - 89,667 views
-			</li>
-			<li>
-				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-colin-mcrae-dirt-3/" title="Save for Colin McRae DiRT 3">Colin McRae DiRT 3</a> - 83,510 views
+				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-dying-light/" title="Save for Dying Light">Dying Light</a> - 94,858 views
 			</li>
 		</ul>
 	<!--/noindex-->

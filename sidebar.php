4f0cb2f04faa29f4a65adcdaabeb06fab4498113
termitkin <bigos911@gmail.com>
2022-12-31 10:07:55 +0300

    Поменял топ сохранений в сайд баре

--- sidebar.php
+++ sidebar.php
@@ -1,36 +1,36 @@
 <aside role="complementary" class="sidebar">
 	<!--noindex-->
-		<div class="popular-posts-heading">Most Popular Savegame</div>
+		<div class="popular-posts-heading">Most Popular Savegames</div>
 		<ul class="popular-posts-list">
 			<li>
-				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-nfs-underground-2/" title="Save for NFS Underground 2">NFS Underground 2</a> - 536,838 views
-			</li>
-			<li>
-				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-test-drive-unlimited-2/" title="Save for Test Drive Unlimited 2">Test Drive Unlimited 2</a> - 316,590 views
+				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-nfs-underground-2/" title="Save for NFS Underground 2">NFS Underground 2</a>
 			</li>
             <li>
-                <a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-grand-theft-auto-5/" title="Save for Grand Theft Auto 5">Grand Theft Auto 5</a> - 313,912 views
+                <a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-grand-theft-auto-5/" title="Save for Grand Theft Auto 5">Grand Theft Auto 5</a>
             </li>
 			<li>
-				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-mafia-2/" title="Save for Mafia 2">Mafia 2</a> - 201,979 views
+				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-test-drive-unlimited-2/" title="Save for Test Drive Unlimited 2">Test Drive Unlimited 2</a>
 			</li>
 			<li>
-				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-bully-scholarship-edition/" title="Save for Bully Scholarship Edition">Bully Scholarship Edition</a> - 190,128 views
-			</li>
-            <li>
-                <a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-assassins-creed-3/" title="Save for Assassin’s Creed 3">Assassin&apos;s Creed 3</a> - 111,354 views
-            </li>
-            <li>
-                <a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-devil-may-cry-4/" title="Save for Devil May Cry 4">Devil May Cry 4</a> - 110,076 views
-            </li>
-            <li>
-                <a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-batman-arkham-city/" title="Save for Batman Arkham City">Batman Arkham City</a> - 104,943 views
-            </li>
-			<li>
-				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-assassins-creed-unity/" title="Save for Assassin’s Creed: Unity">Assassin&apos;s Creed: Unity</a> - 99,821 views
+				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-mafia-2/" title="Save for Mafia 2">Mafia 2</a>
 			</li>
 			<li>
-				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-dying-light/" title="Save for Dying Light">Dying Light</a> - 94,858 views
+				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-bully-scholarship-edition/" title="Save for Bully Scholarship Edition">Bully Scholarship Edition</a>
+			</li>
+            <li>
+                <a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-assassins-creed-3/" title="Save for Assassin’s Creed 3">Assassin&apos;s Creed 3</a>
+            </li>
+            <li>
+                <a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-devil-may-cry-4/" title="Save for Devil May Cry 4">Devil May Cry 4</a>
+            </li>
+            <li>
+                <a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-batman-arkham-city/" title="Save for Batman Arkham City">Batman Arkham City</a>
+            </li>
+			<li>
+				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-my-summer-car/" title="Save for My Summer Car">My Summer Car</a>
+			</li>
+			<li>
+				<a target="_blank" rel="nofollow noopener" class="link" href="//savesforgames.com/game-saves/save-for-guitar-hero-3-legends-of-rock/" title="Save for Guitar Hero 3 Legends Of Rock">Guitar Hero 3 Legends Of Rock</a>
 			</li>
 		</ul>
 	<!--/noindex-->
